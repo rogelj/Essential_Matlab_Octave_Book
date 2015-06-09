b045a03bfbd669148e477cd21cb7e760093d4452
J Rogel-Salazar <j.rogel.datascience@gmail.com>
2015-06-10 00:15:50 +0100

    Chapter 1 - Scripts 11-12
    Chapter 1 - Scripts 11-12

---
+++ Chapter1_EssentialEssentials/1_011_VariableAssignation.m
@@ -0,0 +1,13 @@
+% We can assign values to a variable
+x=-5 
+   
+% $$$ x = 
+% $$$    -5
+
+% We can supress output with a semicolon
+x=-5; y = 9*x, z = x^3+y
+
+% $$$ y = 
+% $$$    -45
+% $$$ z =
+% $$$    -170
---
+++ Chapter1_EssentialEssentials/1_012_Trigonometric.m
@@ -0,0 +1,20 @@
+% We can use a number of mathematical functions
+% such as the trigonometric functions
+x = 3*cos(pi/4), 
+y = 16*sin(pi/3) 
+
+% $$$ x =
+% $$$    2.1213
+% $$$ y =
+% $$$    13.8564
+
+% And the inverse trigonometric functions:
+
+acos(x/3)
+
+asin(y/16) 
+
+% $$$ ans =  
+% $$$    0.78540
+% $$$ ans =  
+% $$$    1.0472
