c7fc775d2332ebc5ef27c47af5b9a1d908e72163
J Rogel-Salazar <j.rogel.datascience@gmail.com>
2015-06-13 17:59:25 +0100

    Script 17
    Script 17

---
+++ Chapter1_EssentialEssentials/1_017_whos.m
@@ -0,0 +1,14 @@
+% The whos command lets us see the contents of the workspace
+
+a=1; 
+x=[1,3]; 
+y=magic(5);
+
+whos
+
+% $$$ 
+% $$$   Name      Size            Bytes  Class 
+% $$$ 
+% $$$   a         1x1                 8  double              
+% $$$   x         1x2                16  double              
+% $$$   y         5x5               200  double 
