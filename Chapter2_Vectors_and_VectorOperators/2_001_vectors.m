0116218df28ae20ba9036415716524023b89b218
J Rogel-Salazar <j.rogel.datascience@gmail.com>
2015-06-19 09:02:23 +0100

    Script 1
    Script 1

---
+++ Chapter2_Vectors_and_VectorOperators/2_001_vectors.m
@@ -0,0 +1,16 @@
+% Let us define some vectors:
+
+vector1 = [3 42, sqrt(25)]
+
+vector2 = [8+9 5-4]
+
+vector3 = [8+9 5 -4]
+
+% $$$ vector1 =
+% $$$    3   42    5
+% $$$ 
+% $$$ vector2 =
+% $$$   17    1
+% $$$ 
+% $$$ vector3 =
+% $$$   17   5   -4
