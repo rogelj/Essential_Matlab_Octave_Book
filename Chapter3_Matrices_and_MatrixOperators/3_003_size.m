b3f7ccbe941f9ddb74632358380b4f9dd27894d9
J Rogel-Salazar <j.rogel.datascience@gmail.com>
2016-07-12 08:42:02 +0100

    Matrix definitions
    Matrix definitions

---
+++ Chapter3_Matrices_and_MatrixOperators/3_003_size.m
@@ -0,0 +1,12 @@
+size(A)
+
+% ans = 3  2
+
+size(B,1)
+
+% ans = 3
+
+size(B,2)
+
+% ans =
+%   2
---
+++ Chapter3_Matrices_and_MatrixOperators/3_004_assignment.m
@@ -0,0 +1,7 @@
+[rows cols]=size(C)
+
+% Given the matrix C from file 3_001_matrices.m
+
+%rows = 3
+
+%cols = 3
